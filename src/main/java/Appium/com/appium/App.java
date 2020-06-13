16b43c39dd5747312ae75face23c9e6b5af4f71c
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-06-13 15:30:38 -0500

    Set the MobileCapabilityType

--- src/main/java/Appium/com/appium/App.java
+++ src/main/java/Appium/com/appium/App.java
@@ -1,16 +1,15 @@
 package Appium.com.appium;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.remote.MobileCapabilityType;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
-        desiredCapabilities.setCapability("", "");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_One_API_29");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "Nexus_One_API_29");
     }
 }
