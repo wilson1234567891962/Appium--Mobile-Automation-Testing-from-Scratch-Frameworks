4060a7d6d9080ce397cdac2937416da79121b64e
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-06-13 15:13:14 -0500

    Implement the base of project

---
+++ src/main/java/Appium/com/appium/App.java
@@ -0,0 +1,16 @@
+package Appium.com.appium;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
+        desiredCapabilities.setCapability("", "");
+    }
+}
---
+++ src/test/java/Appium/com/appium/AppTest.java
@@ -0,0 +1,20 @@
+package Appium.com.appium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
