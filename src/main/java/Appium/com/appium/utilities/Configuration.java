19bebb12b9dade5a75c4cd3da87dc2a08eb4abb0
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-06-13 16:37:24 -0500

    Refactor the codes and method

--- src/main/java/Appium/com/appium/utilities/Configuration.java
+++ src/main/java/Appium/com/appium/utilities/Configuration.java
@@ -1,24 +1,35 @@
 package Appium.com.appium.utilities;
 
-import java.io.File;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.Properties;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Configuration {
-	
+
+	final static Logger logger = Logger.getLogger(Configuration.class);
+
 	public static AndroidDriver<AndroidElement> initConfiguration() throws MalformedURLException {
-		File file = new File("resources");
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_One_API_29");
-		cap.setCapability(MobileCapabilityType.APP, file.getAbsolutePath() + "/com/appium/app/original.apk");
-		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
-		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		AndroidDriver<AndroidElement> driver = null;
+		try (InputStream input = Configuration.class
+				.getResourceAsStream("/com/appium/app/properties/Capabilities.properties")) {
+			Properties properties = new Properties();
+			properties.load(input);
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability(MobileCapabilityType.DEVICE_NAME,  properties.getProperty("capabilities.device.name"));
+			cap.setCapability(MobileCapabilityType.APP,  properties.getProperty("capabilities.app.path"));
+			cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, properties.getProperty("capabilities.app.test"));
+			driver = new AndroidDriver<>(new URL(properties.getProperty("capabilities.server.path")), cap);
+		} catch (IOException ex) {
+			logger.error(ex);
+		}
 		return driver;
 	}
 }
