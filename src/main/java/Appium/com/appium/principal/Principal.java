8f99dccc97d6d58ca56d31d4e8691a3a0055f015
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-06-14 16:15:00 -0500

    Implement the example with AndroidUIAutomator

--- src/main/java/Appium/com/appium/principal/Principal.java
+++ src/main/java/Appium/com/appium/principal/Principal.java
@@ -7,29 +7,36 @@ import Appium.com.appium.utilities.Configuration;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class Principal {
+public class Principal extends Configuration {
 
 	final static Logger logger = Logger.getLogger(Principal.class);
 	
 	public static void main(String[] args) {
 		try {
-			 AndroidDriver<AndroidElement>  driver= Configuration.initConfiguration();
-			 testOne(driver);
+			 testOne();
+			 testTwo();
 		} catch (MalformedURLException e) {
 			logger.error(e);
 		}
 	}
 	
-	private static void testOne(AndroidDriver<AndroidElement>  driver) {
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+	private static void testOne() throws MalformedURLException {
+		 AndroidDriver<AndroidElement>  driver= initConfiguration();
+		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		 driver.findElementsByClassName("android.widget.Button").get(0).click();
 		 driver.findElementByXPath("//android.widget.TextView[@text= 'Preference']").click();
 		 driver.findElementByXPath("//android.widget.TextView[@text= '3. Preference dependencies']").click();
-			
 		 driver.findElementById("android:id/checkbox").click();
 		 driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
 		 driver.findElementByClassName("android.widget.EditText").sendKeys("prueba");
 		 driver.findElementsByClassName("android.widget.Button").get(1).click();
+	}
+	
+	private static void testTwo() throws MalformedURLException {
+		AndroidDriver<AndroidElement>  driver= initConfiguration();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.findElementsByClassName("android.widget.Button").get(0).click();
+		driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+		 System.out.println(driver.findElementsByAndroidUIAutomator("new UiSelector().clickable(true)").size()); 
 	}
 }
